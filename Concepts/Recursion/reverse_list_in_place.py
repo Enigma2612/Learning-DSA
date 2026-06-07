3e918de13511723d076ec1465a3ab5ecc2de1cf0
Enigma2612 <shauryagupta2612@gmail.com>
2026-06-07 21:54:38 +0530

    added note to one problem

--- Concepts/Recursion/reverse_list_in_place.py
+++ Concepts/Recursion/reverse_list_in_place.py
@@ -7,4 +7,8 @@ def reverse(arr, N):
     reverse(arr, N-1)
     arr.append(arr[len(arr) - N])
     arr.pop(len(arr) - N - 1)
+
+#IMPORTANT: Issue here: Popping from the middle of the list, is O(N)! 
+#So Time complexity becomes O(N^2), because of the popping step
+#I'll try to use element swapping somehow, instead of using insertion / deletion
     
