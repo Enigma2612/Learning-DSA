125927daffeb9f253fc916d51e067b1db458b53b
Enigma2612 <shauryagupta2612@gmail.com>
2026-06-21 12:21:56 +0530

    added another question

---
+++ Neetcode/Stack/Car Fleet/sol1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        times = [(position[i], (target - position[i])/speed[i]) for i in range(len(position))]
+        stack = []
+        times.sort()
+        times.reverse()
+        times = [t[1] for t in times]
+
+        for t in times:
+            if not stack:
+                stack.append(t)
+            if stack[-1] < t:
+                stack.append(t)
+        print(stack)       
+        return len(stack)          
+
+#Coded this on my phone while waiting before a meeting lmao
