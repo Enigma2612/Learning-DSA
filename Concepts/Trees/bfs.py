f90cc3cfa9ae9737679fbb023b6ad2da8e026760
Enigma2612 <shauryagupta2612@gmail.com>
2026-07-12 02:13:59 +0530

    added bfs basics

---
+++ Concepts/Trees/bfs.py
@@ -0,0 +1,17 @@
+from sandbox import *
+
+def bfs(root):
+    ans = []
+    queue = [root]
+    l = 0
+    while l < len(queue):
+        el = queue[l]
+        ans.append(el.val)
+        if el.left: queue.append(el.left)
+        if el.right: queue.append(el.right)
+        l += 1
+    return ans
+
+tree_1 = make_tree([1,2,3,4,5,10])
+
+print(bfs(tree_1))
